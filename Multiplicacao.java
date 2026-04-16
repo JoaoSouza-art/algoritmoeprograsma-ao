eee16b84612fcdde19c8328d9388f9dcc3f737b3
JoaoSouza-art <carvalhojoaovitor82@gmail.com>
2026-04-16 00:29:50 +0000

    Aula 15/04

---
+++ Multiplicacao.java
@@ -0,0 +1,11 @@
+void main() {
+
+    int n1 = Integer.parseInt(IO.readln("Digite o primeiro número "));
+    int n2 = Integer.parseInt(IO.readln("Digite o primeiro número "));
+    
+
+    int multiplicacao = n1 * n2;
+
+    IO.println(multiplicacao);
+
+}
